8cd6f5e1e4e81880bccf3fc60e71d660f2e659df
luc_jaubert <j.luc@obvy-app.com>
2024-09-26 14:57:30 +0200

    title effects & header Ok

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -49,42 +49,43 @@ export class HomepageComponent implements OnInit, AfterViewInit {
     const textWrappers1 = document.querySelectorAll('.ml11 .letters-1');
     textWrappers1.forEach((textWrapper) => {
       if (textWrapper && textWrapper.textContent) {
-        textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+        textWrapper.innerHTML = textWrapper.textContent.replace(
+          /([^\x00-\x80]|\w)/g,
+          "<span class='letter'>$&</span>"
+        );
       }
     });
-
+  
     const textWrappers2 = document.querySelectorAll('.ml11 .letters-2');
     textWrappers2.forEach((textWrapper) => {
       if (textWrapper && textWrapper.textContent) {
-        textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+        textWrapper.innerHTML = textWrapper.textContent.replace(
+          /([^\x00-\x80]|\w)/g,
+          "<span class='letter'>$&</span>"
+        );
       }
     });
-
+  
     const timeline = anime.timeline({ loop: false });
     timeline
       .add({
         targets: '.ml11 .first-line',
-        scaleY: [0, 1],
-        opacity: [0.5, 1],
+        translateX: [
+          0,
+          (() => {
+            const letters1 = document.querySelector('.ml11 .letters-1');
+            return letters1 ? letters1.getBoundingClientRect().width + 5 : 0;
+          })(),
+        ],
         easing: 'easeOutExpo',
-        duration: 1500,
-        delay: 500,
-      })
-      .add({
-        targets: '.ml11 .first-line',
-        translateX: [0, (() => {
-          const letters1 = document.querySelector('.ml11 .letters-1');
-          return letters1 ? letters1.getBoundingClientRect().width + 5 : 0;
-        })()],
-        easing: 'easeOutExpo',
-        duration: 400,
+        duration: 800,
       })
       .add({
         targets: '.ml11 .letters-1 .letter',
         opacity: [0, 1],
         easing: 'easeOutExpo',
         duration: 800,
-        delay: (el: HTMLElement, i: number) => 40 * (i + 1),
+        delay: (el: HTMLElement, i: number) => 180 * (i + 1),
       })
       .add({
         targets: '.ml11 .first-line',
@@ -92,19 +93,17 @@ export class HomepageComponent implements OnInit, AfterViewInit {
         duration: 800,
         easing: 'easeOutExpo',
         complete: () => {
-          this.triggerSecondLine();
+          this.triggerSecondLine(); 
         },
       });
-
-    setTimeout(() => {
-      const dotElement = document.querySelector('.dot') as HTMLElement;
-      if (dotElement) {
-        dotElement.style.left = '0';
-      }
-    }, 4750); 
   }
-
+  
   triggerSecondLine(): void {
+    const dotElement = document.querySelector('.dot') as HTMLElement;
+    if (dotElement) {
+      dotElement.style.left = '0';
+    }
+  
     const lineElement = document.querySelector('.ml11 .second-line') as HTMLElement;
     if (lineElement) {
       lineElement.style.transform = 'translateX(0px)';
@@ -112,42 +111,28 @@ export class HomepageComponent implements OnInit, AfterViewInit {
   
     anime.timeline({ loop: false })
       .add({
-        targets: '.ml11 .second-line',
-        scaleY: [0, 1],
-        opacity: [0.5, 1],
-        easing: 'easeOutExpo',
-        duration: 800,
-      })
-      .add({
-        targets: '.ml11 .second-line',
-        translateX: [0, (() => {
-          const letters2 = document.querySelector('.ml11 .letters-2');
-          return letters2 ? letters2.getBoundingClientRect().width - 1 : 0;
-        })()],
-        easing: 'easeOutExpo',
-        duration: 400,
-      })
-      .add({
         targets: '.ml11 .letters-2',
         opacity: 1,
-        duration: 0,
+        duration: 800,
       })
       .add({
         targets: '.ml11 .letters-2 .letter',
         opacity: [0, 1],
         easing: 'easeOutExpo',
-        duration: 400,
-        delay: (el: HTMLElement, i: number) => 40 * (i + 1),
+        duration: 800,
+        delay: (el: HTMLElement, i: number) => 180 * (i + 1),
       })
       .add({
         targets: '.ml11 .second-line',
         opacity: 0,
+        duration: 800,
         easing: 'easeOutExpo',
         complete: () => {
           this.animateDeveloperTitle();
         },
       });
   }
+  
   
   animateDeveloperTitle(): void {
     gsap.to('.ml13 .letters-3', {
